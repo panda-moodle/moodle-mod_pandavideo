dffaf0d99f71f765975bf9a05dea877248534766
Eduardo Kraus <kraus@eduardokraus.com>
2025-11-19 07:58:03 -0300

    V 1.0.13

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025082903;
+$plugin->version = 2025111900;
+$plugin->release = "1.0.13";
 $plugin->requires = 2021041900;
-$plugin->release = "1.0.12";
 $plugin->component = "mod_pandavideo";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
