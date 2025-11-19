b8e0abf712e676e67eebec4854deb920ed52f740
Eduardo Kraus <kraus@eduardokraus.com>
2025-11-19 07:20:47 -0300

    Opening brace must not be followed by a blank line

--- mod_form.php
+++ mod_form.php
@@ -38,7 +38,6 @@ require_once("{$CFG->dirroot}/course/moodleform_mod.php");
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class mod_pandavideo_mod_form extends moodleform_mod {
-
     /**
      * Defines forms elements
      *
